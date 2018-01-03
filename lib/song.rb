8bcc4f2a065b29ebaaa70523a1d40978279d196b
Neena van de Water <nkuriakose@hotmail.com>
2018-01-03 20:50:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,13 @@ class Song
   end
 
   def self.genre_count
+    genre_count = { }
 
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1  
   end
 
   def self.artist_count
